2bdfb92df0bb4e700208d56758fe349b1687e749
Corentin <corentin.detre@gmail.com>
2013-06-10 18:04:57 +0200

    fin lundi
    get access token

--- gen/com/example/mydeezerradio/R.java
+++ gen/com/example/mydeezerradio/R.java
@@ -30,21 +30,24 @@ public final class R {
         public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000e;
+        public static final int action_settings=0x7f080011;
+        public static final int mainV2_button_connect=0x7f080006;
+        public static final int mainV2_button_disconnect=0x7f080007;
+        public static final int mainV2_textView_nameUser=0x7f080005;
         public static final int main_button_connect=0x7f080004;
         public static final int main_checkBox_rememberMe=0x7f080002;
         public static final int main_editText_email=0x7f080000;
         public static final int main_editText_password=0x7f080001;
         public static final int main_textView_rememberMe=0x7f080003;
-        public static final int songInput_button_return=0x7f080007;
-        public static final int songInput_button_search=0x7f080006;
-        public static final int songInput_editText_song=0x7f080005;
-        public static final int songListening_button_play=0x7f08000a;
-        public static final int songListening_button_return=0x7f08000b;
-        public static final int songListening_imageView_cover=0x7f080008;
-        public static final int songListening_textView_author=0x7f080009;
-        public static final int songSelection_button_return=0x7f08000d;
-        public static final int songSelection_listView_songList=0x7f08000c;
+        public static final int songInput_button_return=0x7f08000a;
+        public static final int songInput_button_search=0x7f080009;
+        public static final int songInput_editText_song=0x7f080008;
+        public static final int songListening_button_play=0x7f08000d;
+        public static final int songListening_button_return=0x7f08000e;
+        public static final int songListening_imageView_cover=0x7f08000b;
+        public static final int songListening_textView_author=0x7f08000c;
+        public static final int songSelection_button_return=0x7f080010;
+        public static final int songSelection_listView_songList=0x7f08000f;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -63,20 +66,22 @@ public final class R {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050001;
         public static final int hello_world=0x7f050000;
+        public static final int mainV2_button_connect=0x7f050007;
+        public static final int mainV2_button_disconnect=0x7f050008;
         public static final int main_button_connect=0x7f050005;
         public static final int main_editText_email=0x7f050003;
         public static final int main_editText_password=0x7f050004;
         public static final int main_textView_rememberMe=0x7f050006;
-        public static final int songInput_button_return=0x7f05000a;
-        public static final int songInput_button_search=0x7f050009;
-        public static final int songInput_editText_song=0x7f050008;
-        public static final int songListening_button_return=0x7f05000e;
-        public static final int songListening_imageButton_play_description=0x7f050010;
-        public static final int songListening_imageView_cover_description=0x7f05000f;
-        public static final int songSelection_button_return=0x7f05000c;
-        public static final int title_activity_song_input=0x7f050007;
-        public static final int title_activity_song_listening=0x7f05000d;
-        public static final int title_activity_song_selection=0x7f05000b;
+        public static final int songInput_button_return=0x7f05000c;
+        public static final int songInput_button_search=0x7f05000b;
+        public static final int songInput_editText_song=0x7f05000a;
+        public static final int songListening_button_return=0x7f050010;
+        public static final int songListening_imageButton_play_description=0x7f050012;
+        public static final int songListening_imageView_cover_description=0x7f050011;
+        public static final int songSelection_button_return=0x7f05000e;
+        public static final int title_activity_song_input=0x7f050009;
+        public static final int title_activity_song_listening=0x7f05000f;
+        public static final int title_activity_song_selection=0x7f05000d;
     }
     public static final class style {
         /** 
--- src/com/example/mydeezerradio/MainActivity.java
+++ src/com/example/mydeezerradio/MainActivity.java
@@ -1,5 +1,13 @@
 package com.example.mydeezerradio;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
@@ -11,26 +19,38 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
-import android.widget.CheckBox;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.deezer.sdk.AsyncDeezerTask;
 import com.deezer.sdk.DeezerConnect;
 import com.deezer.sdk.DeezerConnectImpl;
 import com.deezer.sdk.DeezerError;
+import com.deezer.sdk.DeezerRequest;
 import com.deezer.sdk.DialogError;
 import com.deezer.sdk.DialogListener;
 import com.deezer.sdk.OAuthException;
+import com.deezer.sdk.RequestListener;
 
 public class MainActivity extends Activity {
 
 	public final static String TAG = "com.example.mydeezerradio.Mainactivity";
 	final String defValue = "erreur recuperation données";
+	String userId = "8399072";
 	public final static String name_sharedPref = "com.example.mydeezerradio";
 	SharedPreferences sharedPref;
 	SharedPreferences.Editor sharedPref_editor;
-	final String sharedPref_string_email = "string_email";
-	final String sharedPref_string_password = "string_password";
+	final String sharedPref_boolean_isConnected = "boolean_isConnected";
+	public static String access_token = "";
+
+	/**
+	 * v1 : with shared pref
+	 * 
+	 * public final static String name_sharedPref = "com.example.mydeezerradio";
+	 * SharedPreferences sharedPref; SharedPreferences.Editor sharedPref_editor;
+	 * final String sharedPref_string_email = "string_email"; final String
+	 * sharedPref_string_password = "string_password";
+	 */
 
 	/** Your app Deezer appId. */
 	public final static String APP_ID = "119355";
@@ -45,62 +65,116 @@ public class MainActivity extends Activity {
 	/** DeezerConnect object used for auhtentification or request. */
 	private DeezerConnect deezerConnect = new DeezerConnectImpl(APP_ID);
 
-	// /** DeezerRequestListener object used to handle requests. */
-	// RequestListener handler = new MyDeezerRequestHandler();
-	// /** DeezerTaskRequestListener object used to handle requests. */
-	// RequestListener deezerTaskHandler = new MyDeezerTaskHandler();
+	/** DeezerRequestListener object used to handle requests. */
+	RequestListener requestHandler = new MyDeezerRequestHandler();
+	/** DeezerTaskRequestListener object used to handle requests. */
+	RequestListener deezerTaskHandler = new RequestForNameTaskHandler();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_v2);
-
-		// init sharedPref
 		sharedPref = getSharedPreferences(name_sharedPref, Context.MODE_PRIVATE);
 		sharedPref_editor = sharedPref.edit();
-
-		// if sharedPref have been stocked, retrieve them
-		if (sharedPref.contains(sharedPref_string_email)
-				&& sharedPref.contains(sharedPref_string_password)) {
-			((TextView) findViewById(R.id.main_editText_email))
-					.setText(sharedPref.getString(sharedPref_string_email,
-							defValue));
-			((TextView) findViewById(R.id.main_editText_password))
-					.setText(sharedPref.getString(sharedPref_string_password,
-							defValue));
+		if (!sharedPref.contains(sharedPref_boolean_isConnected)) {
+			sharedPref_editor.putBoolean(sharedPref_boolean_isConnected, false);
 		}
 
+		/**
+		 * v1 : with shared pref
+		 * 
+		 * // init sharedPref sharedPref = getSharedPreferences(name_sharedPref,
+		 * Context.MODE_PRIVATE); sharedPref_editor = sharedPref.edit();
+		 * 
+		 * // if sharedPref have been stocked, retrieve them if
+		 * (sharedPref.contains(sharedPref_string_email) &&
+		 * sharedPref.contains(sharedPref_string_password)) { ((TextView)
+		 * findViewById(R.id.main_editText_email))
+		 * .setText(sharedPref.getString(sharedPref_string_email, defValue));
+		 * ((TextView) findViewById(R.id.main_editText_password))
+		 * .setText(sharedPref.getString(sharedPref_string_password, defValue));
+		 * }
+		 */
+
+	}
+
+	protected void onResume() {
+		super.onResume();
+
+		Log.w("TAG / connected",
+				""
+						+ sharedPref.getBoolean(sharedPref_boolean_isConnected,
+								false));
+
+		if (sharedPref.getBoolean(sharedPref_boolean_isConnected, false)) {
+
+			DeezerRequest request_name = new DeezerRequest("/user/" + userId
+					+ "/name/");
+
+			deezerConnect.requestAsync(request_name, requestHandler);
+
+			// AsyncDeezerTask asyncDeezerTask = new AsyncDeezerTask(
+			// deezerConnect, request_name, requestHandler, "name_user");
+
+			Log.w("Main / onResume", "already connected");
+			((TextView) findViewById(R.id.mainV2_textView_nameUser))
+					.setText("Connected");
+		} else {
+			((TextView) findViewById(R.id.mainV2_textView_nameUser))
+					.setText("Not connected, please log in");
+		}
 	}
 
 	public void main_onClick_connect(View view) {
 		// TODO à refaire avec une vraie version
 
-		// Retrieve data
-		TextView main_textView_email = (TextView) findViewById(R.id.main_editText_email);
-		TextView main_textView_password = (TextView) findViewById(R.id.main_editText_password);
-
-		String main_string_email = String
-				.valueOf(main_textView_email.getText());
-		String main_string_password = String.valueOf(main_textView_password
-				.getText());
-
-		// if the user checked "remember me", remember him (stock data in
-		// sharedPref)
-		if (((CheckBox) findViewById(R.id.main_checkBox_rememberMe))
-				.isChecked()) {
-			sharedPref_editor.putString(sharedPref_string_email,
-					main_string_email);
-			sharedPref_editor.putString(sharedPref_string_password,
-					main_string_password);
-			sharedPref_editor.commit();
-		}
+		/**
+		 * v1 : with shared pref
+		 * 
+		 * // Retrieve data TextView main_textView_email = (TextView)
+		 * findViewById(R.id.main_editText_email); TextView
+		 * main_textView_password = (TextView)
+		 * findViewById(R.id.main_editText_password);
+		 * 
+		 * String main_string_email = String
+		 * .valueOf(main_textView_email.getText()); String main_string_password
+		 * = String.valueOf(main_textView_password .getText());
+		 * 
+		 * // if the user checked "remember me", remember him (stock data in //
+		 * sharedPref) if (((CheckBox)
+		 * findViewById(R.id.main_checkBox_rememberMe)) .isChecked()) {
+		 * sharedPref_editor.putString(sharedPref_string_email,
+		 * main_string_email);
+		 * sharedPref_editor.putString(sharedPref_string_password,
+		 * main_string_password); sharedPref_editor.commit(); }
+		 */
 
 		Log.w("Main / onConnect", "isConnected : " + isConnected());
 
 		// Attempt to connect
 		deezerConnect.authorize(MainActivity.this, PERMISSIONS,
-				new MyDialogHandler());
+				new LoginDialogHandler());
 		Log.w("Main / onConnect", "after authorizing");
+
+	}
+
+	public void mainV2_onClick_connect(View view) {
+		// Attempt to connect
+
+		deezerConnect.authorize(MainActivity.this, PERMISSIONS,
+				new LoginDialogHandler());
+
+	}
+
+	public void mainV2_onClick_disconnect(View view) {
+		sharedPref_editor.putBoolean(sharedPref_boolean_isConnected, false);
+
+		deezerConnect.logout(MainActivity.this);
+
+		Toast.makeText(getApplicationContext(), "disconnected",
+				Toast.LENGTH_SHORT).show();
+		((TextView) findViewById(R.id.mainV2_textView_nameUser))
+				.setText("Déconnecté");
 
 	}
 
@@ -111,6 +185,10 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public boolean isLogged() {
+		return deezerConnect.isSessionValid();
+	}
+
 	public boolean isConnected() {
 		ConnectivityManager conn = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
 		NetworkInfo net = conn.getActiveNetworkInfo();
@@ -118,15 +196,21 @@ public class MainActivity extends Activity {
 	}
 
 	/** Handle DeezerConnect callbacks. */
-	private class MyDialogHandler implements DialogListener {
+	private class LoginDialogHandler implements DialogListener {
 		@Override
 		public void onComplete(final Bundle values) {
 			Log.w("Main / onComplete", "OK");
 
-			Toast toast_complete = new Toast(getApplicationContext());
-			toast_complete.setText("Connected");
-			toast_complete.show();
-			
+			MainActivity.access_token = values.getString("access_token");
+
+			sharedPref_editor.putBoolean(sharedPref_boolean_isConnected, true);
+			sharedPref_editor.commit();
+
+			Toast.makeText(getApplicationContext(), "Connected",
+					Toast.LENGTH_SHORT).show();
+
+			Log.w("oncomplete", access_token);
+
 			// go to the inputSong activity
 			Intent intent = new Intent(getApplicationContext(),
 					SongInputActivity.class);
@@ -136,16 +220,19 @@ public class MainActivity extends Activity {
 
 		@Override
 		public void onDeezerError(final DeezerError deezerError) {
-			Toast toast_error = new Toast(getApplicationContext());
-			toast_error.setText("Connection error");
-			toast_error.show();
-			
+			// Toast toast_error = new Toast(getApplicationContext());
+			// toast_error.setText("Connection error");
+			// toast_error.show();
+
 			Log.w("Main / onDeezerError", "ERROR");
 		}// met
 
 		@Override
 		public void onError(final DialogError dialogError) {
 			Log.w("Main / onError", "ERROR");
+			// Toast toast_error = new Toast(getApplicationContext());
+			// toast_error.setText("Connection error");
+			// toast_error.show();
 		}// met
 
 		@Override
@@ -159,5 +246,91 @@ public class MainActivity extends Activity {
 
 		}// met
 	}// inner class
+
+	private class MyDeezerRequestHandler implements RequestListener {
+		public void onComplete(String response, Object requestId) {
+			// Warning code is not executed in UI Thread
+			if ("user_name".equals(requestId)) {
+				Toast.makeText(getApplicationContext(), "user !",
+						Toast.LENGTH_SHORT).show();
+
+			}
+		}
+
+		public void onIOException(IOException e, Object requestId) {
+			Log.w("Main / requestHandler", "IOException");
+			// TODO. Implement some code to handle error. Warning code is not
+			// executed in UI Thread
+		}
+
+		public void onMalformedURLException(MalformedURLException e,
+				Object requestId) {
+			Log.w("Main / requestHandler", "onMalformedURLException");
+			// TODO. Implement some code to handle error. Warning code is not
+			// executed in UI Thread
+		}
+
+		@Override
+		public void onDeezerError(DeezerError arg0, Object arg1) {
+			Log.w("Main / requestHandler", "onDeezerError : " + arg0.toString());
+
+		}
+
+		@Override
+		public void onOAuthException(OAuthException arg0, Object arg1) {
+			Log.w("Main / requestHandler", "onOAuthException");
+
+		}
+	}// class
+
+	class RequestForNameTaskHandler implements RequestListener {
+		public void onComplete(String response, Object requestId) {
+			// code is executed in UI Thread
+			Toast.makeText(getApplicationContext(),
+					"onComplete deezer task:" + response, Toast.LENGTH_SHORT)
+					.show();
+			if ("list-playlists".equals(requestId)) {
+				// TODO. Implement some code to parse the answer as detailled in
+				// http://developers.deezer.com/api/user/playlists
+			}// if
+		}
+
+		public void onIOException(IOException e, Object requestId) {
+			// TODO. Implement some code to handle error. Code is executed in UI
+			// Thread
+		}
+
+		public void onMalformedURLException(MalformedURLException e,
+				Object requestId) {
+			// TODO. Implement some code to handle error. Code is executed in UI
+			// Thread
+		}
+
+		@Override
+		public void onDeezerError(DeezerError arg0, Object arg1) {
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void onOAuthException(OAuthException arg0, Object arg1) {
+			// TODO Auto-generated method stub
+		}
+	}// inner class
+
+	public void btDeezerASyncTaskRequest(View view) {
+		// sending requests
+
+		String userId = "2529";
+		DeezerRequest request = new DeezerRequest("/user/" + userId
+				+ "/playlists");
+		AsyncDeezerTask asyncDeezerTask = new AsyncDeezerTask(deezerConnect,
+				deezerTaskHandler);
+		// optinionally, use an executor
+		BlockingQueue<Runnable> worksQueue = new ArrayBlockingQueue<Runnable>(2);
+		Executor executor = new ThreadPoolExecutor(3, 3, 10, TimeUnit.SECONDS,
+				worksQueue);
+		// execute the AsyncTask with the executor
+		asyncDeezerTask.executeOnExecutor(executor, request);
+	}
 
 }
